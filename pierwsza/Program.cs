94143b61cba299fb244e428400e335a1a3175b1e
Tomasz Kłosowski <atomasz.k@gmail.com>
2024-01-31 18:09:06 +0100

    Nowe zmiany

--- pierwsza/Program.cs
+++ pierwsza/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj, Tomaszu!");
+﻿var name = "Tomasz";
+var age = 57;
+if (name == "Tomasz" && age < 57)
+{
+    Console.WriteLine("Jestem Tomaszem lub ma mniej niż 60");
+}
+else
+{
+    Console.WriteLine("Jestem kims innym");
+}
+
